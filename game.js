0740febea5b4c423a5502344393b7177fb987db3
takacsberni <takacsberni@gmail.com>
2022-01-11 17:55:46 +0100

    Limit moves of frog between the game board is done

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 initGame();
 
-let frog= document.querySelector('.frog');
+let frog = document.querySelector('.frog');
 let move = 60;
 let container = document.querySelector('.container');
 let borders = container.getBoundingClientRect();
@@ -28,24 +28,34 @@ window.addEventListener('load', () => {
 
 
 window.addEventListener('keyup', (e) => {
-    if ((parseInt(frog.style.left) >= -360 && parseInt(frog.style.left) <= 360) && (parseInt(frog.style.top) >= -300 && parseInt(frog.style.top) <= 0)) {        switch (e.key) {
-            case 'ArrowLeft':
-                frog.style.left = parseInt(frog.style.left) - move + 'px';
-                console.log('Béka left / right:', parseInt(frog.style.left))
-                break;
-            case 'ArrowRight':
-                frog.style.left = parseInt(frog.style.left) + move + 'px';
-                console.log('Béka left / right:', parseInt(frog.style.left))
-                break;
-            case 'ArrowUp':
-                frog.style.top = parseInt(frog.style.top) - move + 'px';
-                console.log('Béka bottom:', parseInt(frog.style.top))
-                break;
-            case 'ArrowDown':
-                frog.style.top = parseInt(frog.style.top) + move + 'px';
-                console.log('Béka bottom:', parseInt(frog.style.top))
-                break;
-        }
+    // add functionality for arrow buttons
+    switch (e.key) {
+        case 'ArrowLeft':
+            frog.style.left = parseInt(frog.style.left) - move + 'px';
+            console.log('Béka left / right:', parseInt(frog.style.left))
+            break;
+        case 'ArrowRight':
+            frog.style.left = parseInt(frog.style.left) + move + 'px';
+            console.log('Béka left / right:', parseInt(frog.style.left))
+            break;
+        case 'ArrowUp':
+            frog.style.top = parseInt(frog.style.top) - move + 'px';
+            console.log('Béka bottom:', parseInt(frog.style.top))
+            break;
+        case 'ArrowDown':
+            frog.style.top = parseInt(frog.style.top) + move + 'px';
+            console.log('Béka bottom:', parseInt(frog.style.top))
+            break;
+    }
+    // limit frog's moves between board
+    if (frog.style.left === '-480px') {
+        frog.style.left = '-420px';
+    } else if (frog.style.left === '480px'){
+        frog.style.left = '420px';
+    } else if (frog.style.top === '-420px'){
+        frog.style.top = '-360px';
+    } else if (frog.style.top === '60px'){
+        frog.style.top = '0px';
     }
 });
 

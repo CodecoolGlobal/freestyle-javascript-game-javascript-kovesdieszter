8f740bb046b0b037cbe947fd8013b4480d53f53b
Hanna Szepesi <hannaszepesi.7@gmail.com>
2022-01-11 15:59:46 +0100

    move cars on board

--- game.js
+++ game.js
@@ -9,21 +9,26 @@ function initGame() {
 }
 
 
-const getPosAll = (element) => {
-    const {x, y, top, right, bottom, left, width, height} = element.getBoundingClientRect();
-    return {x, y, top, right, bottom, left, width, height} }
+//const getPosAll = (element) => {
+  //  const {x, y, top, right, bottom, left, width, height} = element.getBoundingClientRect();
+  //  return {x, y, top, right, bottom, left, width, height} }
 
 
 
-function carMove() {
+async function carMove() {
+
     let carsLeft = document.getElementsByClassName("car-left")
     for (let i = 0; i < carsLeft.length; i++) {
         let car = carsLeft.item(i)
-        while (car.style.left !== "900px") {
+        while (true) {
             let elstyle = window.getComputedStyle(car)
             let carLeft = elstyle.getPropertyValue("left").replace('px', '')
-            setInterval(function () {car.style.left = `${parseInt(carLeft, 10) + 1}px`}, 100)
+            car.style.left = `${parseInt(carLeft, 10) + 1}px`
             console.log(carLeft)
+            await new Promise((r) => setTimeout(() => r(), 10));
+            if (car.style.left === "900px") {
+                car.style.left = "10px"
+            }
         }
     }
 }

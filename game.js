3ca5be9f382b311712d2e706c64e9a85cea7abe8
kovesdieszter <kovesdieszter@github.com>
2022-01-11 10:20:18 +0100

    feat: move circle

--- game.js
+++ game.js
@@ -1,11 +1,36 @@
 initGame();
 
+let circle = document.querySelector('.circle');
+let move = 60;
+
 function initGame() {
     let cars = document.getElementsByClassName('cars');
-    console.log(cars[2]);
+
     for (let i = 0; i < cars.length; i++) {
-        cars[i].setAttribute('style', 'background-color: red;');
+        cars[i].style.backgroundColor = "red";
     }
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
+
+window.addEventListener('load', () => {
+    circle.style.position = 'absolute';
+    circle.style.left = 0;
+    circle.style.top = 0;
+});
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'ArrowLeft':
+            circle.style.left = parseInt(circle.style.left) - move + 'px';
+            break;
+        case 'ArrowRight':
+            circle.style.left = parseInt(circle.style.left) + move + 'px';
+            break;
+        case 'ArrowUp':
+            circle.style.top = parseInt(circle.style.top) - move + 'px';
+            break;
+        case 'ArrowDown':
+            circle.style.top = parseInt(circle.style.top) + move + 'px';
+            break;
+    }
+});

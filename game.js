cc00b57fcb8173b7e123c761d9c082013687741d
Hanna Szepesi <hannaszepesi.7@gmail.com>
2022-01-11 15:31:23 +0100

    move cars on board

--- game.js
+++ game.js
@@ -7,6 +7,26 @@ function initGame() {
     };
     // Your game can start here, but define separate functions, don't write everything in here :
 }
-console.log(document.getElementById("1").getBoundingClientRect());
 
+
+const getPosAll = (element) => {
+    const {x, y, top, right, bottom, left, width, height} = element.getBoundingClientRect();
+    return {x, y, top, right, bottom, left, width, height} }
+
+
+
+function carMove() {
+    let carsLeft = document.getElementsByClassName("car-left")
+    for (let i = 0; i < carsLeft.length; i++) {
+        let car = carsLeft.item(i)
+        while (car.style.left !== "900px") {
+            let elstyle = window.getComputedStyle(car)
+            let carLeft = elstyle.getPropertyValue("left").replace('px', '')
+            setInterval(function () {car.style.left = `${parseInt(carLeft, 10) + 1}px`}, 100)
+            console.log(carLeft)
+        }
+    }
+}
+
+carMove();
 

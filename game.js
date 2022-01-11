ffd0659ac8ed00aaea775954558772259f23e8df
takacsberni <takacsberni@gmail.com>
2022-01-11 16:37:33 +0100

    Removes red background from roads, and swap finish and start

--- game.js
+++ game.js
@@ -13,9 +13,9 @@ console.log(containerTop);
 function initGame() {
     let cars = document.getElementsByClassName('cars');
 
-    for (let i = 0; i < cars.length; i++) {
-        cars[i].style.backgroundColor = "red";
-    }
+    // for (let i = 0; i < cars.length; i++) {
+    //     cars[i].style.backgroundColor = "red";
+    // }
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }

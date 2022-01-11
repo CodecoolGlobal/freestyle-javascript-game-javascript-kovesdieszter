3de75329d67df5009685c2188a30d2fa4526d7b5
takacsberni <takacsberni@gmail.com>
2022-01-11 09:27:31 +0100

    Planning a structure for the game

--- game.js
+++ game.js
@@ -1,7 +1,11 @@
 initGame();
 
 function initGame() {
-
+    let cars = document.getElementsByClassName('cars');
+    console.log(cars[2]);
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].setAttribute('style', 'background-color: red;');
+    }
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }

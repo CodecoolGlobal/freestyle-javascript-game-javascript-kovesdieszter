2875e5907ad7805b63f570e2d7104e864461a895
takacsberni <takacsberni@gmail.com>
2022-01-11 15:09:48 +0100

    Not makes any significant modification, just because the pull

--- game.js
+++ game.js
@@ -1,36 +1,36 @@
 initGame();
 
-let circle = document.querySelector('.circle');
+let frog = document.querySelector('.frog');
 let move = 60;
 
 function initGame() {
     let cars = document.getElementsByClassName('cars');
 
-    for (let i = 0; i < cars.length; i++) {
-        cars[i].style.backgroundColor = "red";
-    }
+    // for (let i = 0; i < cars.length; i++) {
+    //     cars[i].style.backgroundColor = "red";
+    // }
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
 
 window.addEventListener('load', () => {
-    circle.style.position = 'absolute';
-    circle.style.left = 0;
-    circle.style.top = 0;
+    frog.style.position = 'relative';
+    frog.style.left = 0;
+    frog.style.top = 0;
 });
 window.addEventListener('keyup', (e) => {
     switch (e.key) {
         case 'ArrowLeft':
-            circle.style.left = parseInt(circle.style.left) - move + 'px';
+            frog.style.left = parseInt(frog.style.left) - move + 'px';
             break;
         case 'ArrowRight':
-            circle.style.left = parseInt(circle.style.left) + move + 'px';
+            frog.style.left = parseInt(frog.style.left) + move + 'px';
             break;
         case 'ArrowUp':
-            circle.style.top = parseInt(circle.style.top) - move + 'px';
+            frog.style.top = parseInt(frog.style.top) - move + 'px';
             break;
         case 'ArrowDown':
-            circle.style.top = parseInt(circle.style.top) + move + 'px';
+            frog.style.top = parseInt(frog.style.top) + move + 'px';
             break;
     }
 });
